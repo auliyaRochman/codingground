11c5a3779ab2be804c969348ee32f98916e01f4f
auliyaRochman <auliya1405@gmail.com>
2017-02-27 11:11:38 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Tugas1.java
@@ -0,0 +1,9 @@
+public class Tugas1{
+
+public static void main(String []args){
+    String nama = "Auliya R.";
+    String kota = "Jepara";
+    
+    System.out.println("Nama : " +nama +"\nKota : "+kota);
+    }
+}

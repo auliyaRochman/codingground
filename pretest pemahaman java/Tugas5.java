9e054bcc1e035fb35c8ac7a1811a9b34c6d2635f
auliyaRochman <auliya1405@gmail.com>
2017-02-27 11:48:27 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Tugas5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Tugas5{
+
+     public static void main(String []args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukan angka : ");
+        int a = scanner.nextInt();
+        int hasil = HitungMundur(a);
+        for (int i = hasil; i>= 1; i--) {
+         System.out.println(i);
+        }
+     }
+     
+     public static int HitungMundur(int a1) {
+      int angka = a1;
+      
+      return angka; 
+     }
+}
